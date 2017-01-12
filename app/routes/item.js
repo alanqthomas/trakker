7e173f775f849925e7f20635727f33829cf78746
Alan Thomas <alan.thomas@avi.com>
2017-01-12 01:32:44 -0500

    Item add now automatically searches for IMDB ID by item name.

--- app/routes/item.js
+++ app/routes/item.js
@@ -2,13 +2,13 @@ const express = require('express'),
 			router = express.Router(),
 			assert = require('assert'),
 			Item = require('../models/item.js'),
-			passport = require('passport')
+			passport = require('passport'),
+			imdb = require('imdb-api')
 const requireAuth = passport.authenticate('jwt', { session: false })
 
 const route = '/item'
 
 router.get(`${route}`, requireAuth, (req, res) => {
-	console.log('req', req.user)
 	Item.find({'userId': req.user._id}, (err, docs) => {
 		if (err)
 			res.send(err)
@@ -25,27 +25,27 @@ router.get(`${route}/:id`, requireAuth, (req, res) => {
 })
 
 router.post(`${route}`, requireAuth, (req, res) => {
-	console.log('req', req.user._id)
 	const body = req.body;
-	const newItem = new Item({
-		name: body.name,
-		type: body.type,
-		status: body.status,
-		updatedDate: body.updatedDate,
-		progress: body.progress,
-		imageURL: body.imageURL,
-		imdb: body.imdb,
-		userId: req.user._id
-	}).save((err, result) => {
-		if (err)
-			res.send(err)
-		console.log('Inserted a document')
-		res.sendStatus(201)
+	imdb.getReq({"name": body.name}, (err, data) => {
+		const newItem = new Item({
+			name: body.name,
+			type: body.type,
+			status: body.status,
+			updatedDate: body.updatedDate,
+			progress: body.progress,
+			imageURL: body.imageURL,
+			imdb: data ? data.imdbid: null,
+			userId: req.user._id
+		}).save((err, result) => {
+			if (err)
+				res.send(err)
+			console.log('Inserted a document')
+			res.sendStatus(201)
+		})
 	})
 })
 
 router.delete(`${route}/:id`, requireAuth, (req, res) => {
-	console.log('req', req.user)
 	Item.find({'_id': req.params.id, 'userId': req.user._id}).remove( (err, docs) => {
 		if (err)
 			res.send(err)
@@ -55,7 +55,6 @@ router.delete(`${route}/:id`, requireAuth, (req, res) => {
 })
 
 router.put(`${route}/:id`, requireAuth, (req, res) => {
-	console.log('req', req.user)
 	delete req.body._id
 	console.log(`Updating object with id ${req.params.id} to the object:`, req.body)
 	const body = req.body
